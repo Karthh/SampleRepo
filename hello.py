872eaa1a9a0c207503d1ea037b485781905fc44a
Karthh <scorpiokarth@gmail.com>
2018-07-27 01:17:07 -0400

    Python Changes

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import pandas as pd
+
+d = {'one' : pd.Series([100.,200.,300.], index = ['apple', 'ball', 'clock']),
+ 'two' : pd.Series([111.,222.,333.,4444], index = ['apple', 'ball', 'cerill', 'dancy'])}
+
+df = pd.DataFrame(d)
+df['three'] = df['one'] * df['two']
+df['flag'] = df['one']>250
+df
